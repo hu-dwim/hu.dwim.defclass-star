7b555eae26b3040cf3c26c79033b0f2e662e196f
Artyom Bologov <aartaka@protonmail.com>
2023-04-24 20:45:45 +0400

    Document auto-generated slot accessors.
    This forces (setf-s) documentation to return the respective
    documentation for the function corresponding to slot accessor.
    
    This is not particularly an erroneous behavior if the accessor is
    undocumented, but it eases the burden of NASDF undocumented exports
    checking, where we're using too much hacks to discover slot
    accessors. And it's nicer to have some documentation to accessor than
    to have none.

--- source/defclass-star.lisp
+++ source/defclass-star.lisp
@@ -246,6 +246,7 @@ If the slot is a boolean, it ensures the name is suffixed with \"?\"."
     (destructuring-bind (&key (accessor 'missing) (initarg 'missing)
                               (reader 'missing) (writer 'missing)
                               (export 'missing) (type 'missing)
+                              documentation
                               &allow-other-keys)
         definition
       (remf-keywords definition :accessor :reader :writer :initform :initarg :export :type)
@@ -341,7 +342,31 @@ If the slot is a boolean, it ensures the name is suffixed with \"?\"."
                     (when (and accessor (not (eq accessor 'missing)))
                       (push accessor *symbols-to-export*)))
                   (when *export-slot-names-p*
-                    (push name *symbols-to-export*))))))))))
+                    (push name *symbols-to-export*))))
+            (let* ((accessor-p (or (provided-p accessor)
+                                   (and (provided-p writer)
+                                        (provided-p reader))))
+                   (documented-symbol (cond
+                                        (accessor-p accessor)
+                                        ((provided-p writer)
+                                         (second writer))
+                                        ((provided-p reader)
+                                         reader))))
+              (when (and documented-symbol
+                         (not (documentation documented-symbol 'function)))
+                (setf (documentation documented-symbol 'function)
+                      (format nil "Auto-generated ~a function for slot ~a.~@[
+~a~]"
+                              (cond
+                                (accessor-p "accessor")
+                                ((and (provided-p writer)
+                                      (not (provided-p reader)))
+                                 "writer")
+                                ((and (provided-p reader)
+                                      (not (provided-p writer)))
+                                 "reader")
+                                (t "accessor"))
+                              name documentation))))))))))
 
 (defun extract-options-into-bindings (options)
   (let ((binding-names)
